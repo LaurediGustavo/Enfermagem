c7241b68739b442b113f92d85d04209e45c5d593
Lauredi Gustavo <68924576+LaurediGustavo@users.noreply.github.com>
2021-06-13 14:57:58 -0300

    commit inital

---
+++ Js/Topico.js
@@ -0,0 +1,43 @@
+mudarPorTamanho('');
+
+function show(n) {
+
+    var id = "celula" + n;
+    var sp = "sp" + n;
+
+    var div = document.getElementById(id).style.display;
+
+    if (div == 'none'){
+        document.getElementById(id).style.display = 'block';
+        document.getElementById(sp).innerText = '-';
+    }
+    else {
+        document.getElementById(id).style.display = 'none';
+        document.getElementById(sp).innerText = '+';
+    }
+
+    //Serve para deixar o item selecionado
+    //fixado no topo
+    mudarPorTamanho(n);
+}
+
+function mudarPorTamanho(n) {
+    if (screen.width < 768){
+        var id = "lista" + n;
+
+        if (document.querySelector('.container').clientHeight > screen.height - 50){
+            document.getElementById('footer').style.position = 'relative';
+        }
+        else {
+            document.getElementById('footer').style.position = 'absolute';
+        }
+    }
+    else {
+        if (document.querySelector('.container').clientHeight > screen.height - 240){
+            document.getElementById('footer').style.position = 'relative';
+        }
+        else {
+            document.getElementById('footer').style.position = 'absolute';
+        }
+    }
+}
